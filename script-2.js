172374121d06682d21f856b4717952cec7043857
Jens Obel <jensobel@me.com>
2017-05-19 11:42:08 +0200

    Fixed problems with files

--- script-2.js
+++ script-2.js
@@ -148,16 +148,6 @@ function drawCircles(myData, x, className) {
             return rScale(d.oneYear)
         })
 
-    // var rSize = (function(d) {
-    //         return rScale(d.oneYear)
-    //       });
-    //
-    // if (rSize<5) {
-    //       rSize = 5
-    //     }
-
-    console.log(rSize);
-
     myCircles.enter()
         .append("circle")
         .attr("cx", 380)
@@ -165,9 +155,9 @@ function drawCircles(myData, x, className) {
         .attr("class", "greenhousegas " + className)
         // .attr("stroke-width", 15)
         // .attr("fill", "none")
-        .attr("r", (function(d) {
-                return rScale(d.oneYear)
-              }););
+        .attr("r", function(d) {
+            return rScale(d.oneYear)
+        });
 }
 
 function drawLabels(myData, x) {
